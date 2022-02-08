4e675024a2f9207bac614c52017453379c28db50
ddmoyu <daydaymoyu@gmail.com>
2022-02-08 20:32:19 +0800

    fix: eslint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     '@typescript-eslint/no-var-requires': 0,
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off'
   }
 }
