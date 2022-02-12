afd2a8dfeaa3bffed21a22143aa5a33d695a896f
zzwwllmy <zzwwllmy@gmail.com>
2022-02-12 10:30:37 +0800

    feat:add DB init decorator

--- src/renderer/pages/main/App.vue
+++ src/renderer/pages/main/App.vue
@@ -20,8 +20,10 @@
 </template>
 <script lang="ts" setup>
 import { settingsDB } from '@/renderer/utils/database/controller/settingsDB'
+import { History } from '@/renderer/utils/database/models/History'
 import { darkTheme, lightTheme } from 'naive-ui'
 import bus from './plugins/mitt'
+console.log(new History())
 const activeTheme = ref(darkTheme)
 onMounted(async () => {
   const res = await settingsDB.getSetting('theme')
--- src/renderer/utils/database/models/History.ts
+++ src/renderer/utils/database/models/History.ts
@@ -1,24 +1,52 @@
+import { Init, InitAble } from '../decorator'
 
+@InitAble
 export class History {
   id!: number
-  site: string
-  ids: number
-  name: string
-  type: string
-  year: string
-  index: number
-  time: string
-  detail: string
-  constructor () {
-    this.site = ''
-    this.ids = -1
-    this.name = ''
-    this.type = ''
-    this.year = ''
-    this.index = -1
-    this.time = ''
-    this.detail = ''
-  }
+  @Init('')
+  site!: string
+
+  @Init(0)
+  ids!: number
+
+  @Init('')
+  name!: string
+
+  @Init('')
+  type!: string
+
+  @Init('')
+  year!: string
+
+  @Init(-1)
+  index!: number
+
+  @Init('')
+  time!: string
+
+  @Init('')
+  detail!: string
 }
+// export class History {
+//   id!: number
+//   site: string
+//   ids: number
+//   name: string
+//   type: string
+//   year: string
+//   index: number
+//   time: string
+//   detail: string
+//   constructor () {
+//     this.site = ''
+//     this.ids = -1
+//     this.name = ''
+//     this.type = ''
+//     this.year = ''
+//     this.index = -1
+//     this.time = ''
+//     this.detail = ''
+//   }
+// }
 // use in indexDB stores init
 export const HistoryStores = 'id++, site, ids, name, type, year, index, time, detail'
