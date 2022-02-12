ed01fcd44df7dc3e75ce9bf83d77fb402a31b4e3
ddmoyu <daydaymoyu@gmail.com>
2022-02-12 15:36:24 +0800

    feat: player style

--- src/renderer/pages/main/views/play/index.vue
+++ src/renderer/pages/main/views/play/index.vue
@@ -1,15 +1,46 @@
 <template>
-  <div id="play" class="play">play</div>
+  <div class="header"></div>
+  <div class="body">
+    <div id="player" class="player"></div>
+  </div>
+  <div class="footer"></div>
 </template>
 <script lang="ts" setup>
-import 'xgplayer'
+import { IPlayerOptions } from 'xgplayer'
 import HLS from 'xgplayer-hls.js'
+
+let player
+const config = ref<IPlayerOptions>({
+  id: 'player',
+  url: 'https://v1.cdtlas.com/20210927/cECAezBY/index.m3u8',
+  lang: 'en',
+  width: '100%',
+  height: '100%',
+  pip: true,
+  autoplay: false,
+  videoInit: true,
+  airplay: true,
+  defaultPlaybackRate: 1,
+  playbackRate: [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 3, 4, 5]
+})
+
+function init () {
+  player = new HLS(config.value)
+  // player.src = ''
+}
+
 onMounted(() => {
-  const p = new HLS({
-    id: 'play',
-    url: ''
-  })
-  console.log(p)
+  init()
 })
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.header{
+  height: 40px;
+}
+.body{
+  flex: 1;
+}
+.footer{
+  height: 40px;
+}
+</style>
