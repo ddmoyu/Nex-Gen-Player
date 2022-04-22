a27425967060d640dc3dd90d925fa00661f1434b
ddmoyu <daydaymoyu@gmail.com>
2022-04-22 18:38:44 +0800

    feat: modify about page

--- src/renderer/pages/main/views/menu/about.vue
+++ src/renderer/pages/main/views/menu/about.vue
@@ -1,12 +1,37 @@
 <template>
-  <div class="about">about</div>
+  <div class="about">
+    <n-scrollbar>
+      <div class="wrapper">
+        <div class="logo">logo</div>
+        <div class="name">tv player <span>v0.1.0-alpha.1</span></div>
+        <div class="link">
+          <span>官网</span>
+          <span>Github</span>
+          <span>更新日志</span>
+          <span>常见问题</span>
+          <span>问题反馈</span>
+        </div>
+        <div class="donate">
+          捐助
+        </div>
+        <div class="disclaimers">
+          免责声明
+        </div>
+      </div>
+    </n-scrollbar>
+  </div>
 </template>
 <script lang="ts" setup></script>
 <style lang="scss" scoped>
 .about{
   flex: 1;
   display: flex;
-  flex-direction: column;
-  position: relative;
+  padding: 10px;
+  overflow-y: auto;
+  .wrapper{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 }
 </style>
