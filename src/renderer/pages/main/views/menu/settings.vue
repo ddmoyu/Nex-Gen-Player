af70a1b34ddb56e9777367c7f7cc05791d2c9d52
ddmoyu <daydaymoyu@gmail.com>
2022-04-22 12:00:07 +0800

    feat: settings page style

--- src/renderer/pages/main/views/menu/settings.vue
+++ src/renderer/pages/main/views/menu/settings.vue
@@ -1,28 +1,83 @@
 <template>
   <div class="settings">
-    <div>
-      <n-button @click="changeTheme('dark')">Dark Theme</n-button>
-      <n-button @click="changeTheme('light')">light Theme</n-button>
-    </div>
-    <div>
-      <n-button @click="changeLanguage('en-US')">en language</n-button>
-      <n-button @click="changeLanguage('zh-CN')">zh language</n-button>
-    </div>
+    <div class="header">Settings</div>
+    <n-divider />
+    <n-collapse :default-expanded-names="['1', '2']">
+      <n-collapse-item title="Theme" name="1">
+        <n-radio-group v-model:value="theme" @change="changeTheme">
+          <n-radio-button :value="'light'" label="light" />
+          <n-radio-button :value="'dark'" label="dark" />
+          <n-radio-button :value="'system'" label="system" />
+        </n-radio-group>
+      </n-collapse-item>
+      <n-collapse-item title="Language" name="2">
+        <n-radio-group v-model:value="language" @change="changeLanguage">
+          <n-radio-button :value="'zh-CN'" label="zh-CN" />
+          <n-radio-button :value="'en-US'" label="en-US" />
+        </n-radio-group>
+      </n-collapse-item>
+      <n-collapse-item title="External Player" name="3">
+        <n-input-group>
+          <n-input v-model:value="player" :style="{ width: '50%' }" disabled /><n-button type="primary" @click="changePath('player')">Edit</n-button>
+        </n-input-group>
+      </n-collapse-item>
+      <n-collapse-item title="External Downloader" name="4">
+        <n-input-group>
+          <n-input v-model:value="downloader" :style="{ width: '50%' }" disabled /><n-button type="primary" @click="changePath('downloader')">Edit</n-button>
+        </n-input-group>
+      </n-collapse-item>
+      <n-collapse-item title="Shortcut" name="5">
+        <div>可以</div>
+      </n-collapse-item>
+    </n-collapse>
   </div>
 </template>
 <script lang="ts" setup>
+import { IpcDirective } from '@/main/ipcEnum'
+import { settingsDB } from '@/renderer/utils/database/controller/settingsDB'
 import { useI18n } from 'vue-i18n'
 import bus from '../../plugins/mitt'
 
 const { locale } = useI18n()
+const theme = ref('light')
+const language = ref('en-US')
+const player = ref('')
+const downloader = ref('')
 
-function changeTheme (theme: string) {
-  bus.emit('bus.settings.theme', theme)
+async function initSettings () {
+  theme.value = await settingsDB.getSetting('theme')
+  language.value = await settingsDB.getSetting('language')
+  player.value = await settingsDB.getSetting('player')
+  downloader.value = await settingsDB.getSetting('downloader')
 }
 
-function changeLanguage (lang: string) {
-  locale.value = lang
+function changeTheme () {
+  bus.emit('bus.settings.theme', theme.value)
 }
+
+function changeLanguage () {
+  locale.value = language.value
+  settingsDB.updateSetting({ language: language.value })
+}
+
+function changePath (type: string) {
+  window.ipc.invoke(IpcDirective.WIN_DIALOG)
+  window.ipc.once(IpcDirective.WIN_DIALOG_REPLAY, (e, args) => {
+    if (args.canceled) return false
+    const path = args.filePaths[0]
+    if (type === 'player') {
+      player.value = path
+      settingsDB.updateSetting({ player: path })
+    } else if (type === 'downloader') {
+      downloader.value = path
+      settingsDB.updateSetting({ downloader: path })
+    }
+  })
+}
+
+onMounted(() => {
+  initSettings()
+})
 </script>
 <style lang="scss" scoped>
 .settings{
@@ -30,5 +85,22 @@ function changeLanguage (lang: string) {
   display: flex;
   flex-direction: column;
   position: relative;
+  padding: 10px;
+  .header{
+    font-size: 24px;
+    font-weight: bold;
+  }
+  .item{
+    display: flex;
+    height: 60px;
+    align-items: center;
+    .label{
+      width: 220px;
+      font-size: 18px;
+    }
+    .seting-item{
+      flex: 1;
+    }
+  }
 }
 </style>
--- src/renderer/utils/database/models/Setting.ts
+++ src/renderer/utils/database/models/Setting.ts
@@ -6,13 +6,13 @@ export class Setting {
   readonly view?: string
   readonly windows?: boolean
   readonly play?: {[key:string]:any}
-  readonly theme: 'dark' | 'light'
+  readonly theme: 'dark' | 'light' | 'system'
   readonly language: string
   readonly os?: string
   readonly shortcut?: {[key:string]:any}
   readonly search?: string
   readonly player?: string
-  readonly downlaoder?: string
+  readonly downloader?: string
   readonly exclude?: {[key:string]:any}
   readonly proxy?: {[key:string]:any}
   readonly password?: {[key:string]:any}
