3d95f4574655948ad45ed7f10a88acf3ad00320f
zzwwllmy <zzwwllmy@gmail.com>
2022-05-13 18:04:25 +0800

    feat:intergation register fn

--- src/renderer/components/Aside.vue
+++ src/renderer/components/Aside.vue
@@ -3,71 +3,119 @@
     <div class="top">
       <n-popover trigger="hover" placement="right">
         <template #trigger>
-          <n-button circle type="primary" size="large" @click="goView('discovery')" :dashed="active === 'discovery'"
-            :quaternary="active !== 'discovery'">
+          <n-button
+            circle
+            type="primary"
+            size="large"
+            @click="goView('discovery')"
+            :dashed="active === 'discovery'"
+            :quaternary="active !== 'discovery'"
+          >
             <n-icon size="20">
               <PrismSharp />
             </n-icon>
           </n-button>
         </template>
-        <span>{{ $t('discovery') }}</span>
+        <span>{{ $t("discovery") }}</span>
       </n-popover>
       <n-popover trigger="hover" placement="right">
         <template #trigger>
-          <n-button circle type="primary" size="large" @click="goView('iptv')" :dashed="active === 'iptv'"
-            :quaternary="active !== 'iptv'">
+          <n-button
+            circle
+            type="primary"
+            size="large"
+            @click="goView('iptv')"
+            :dashed="active === 'iptv'"
+            :quaternary="active !== 'iptv'"
+          >
             <n-icon size="20">
               <Tv />
             </n-icon>
           </n-button>
         </template>
-        <span>{{ $t('tv') }}</span>
+        <span>{{ $t("tv") }}</span>
       </n-popover>
       <n-popover trigger="hover" placement="right">
         <template #trigger>
-          <n-button circle type="primary" size="large" @click="goView('play')" :dashed="active === 'play'"
-            :quaternary="active !== 'play'">
+          <n-button
+            circle
+            type="primary"
+            size="large"
+            @click="goView('play')"
+            :dashed="active === 'play'"
+            :quaternary="active !== 'play'"
+          >
             <n-icon size="20">
               <Play />
             </n-icon>
           </n-button>
         </template>
-        <span>{{ $t('play') }}</span>
+        <span>{{ $t("play") }}</span>
       </n-popover>
       <n-popover trigger="hover" placement="right">
         <template #trigger>
-          <n-button circle type="primary" size="large" @click="goView('favorites')" :dashed="active === 'favorites'"
-            :quaternary="active !== 'favorites'">
+          <n-button
+            circle
+            type="primary"
+            size="large"
+            @click="goView('favorites')"
+            :dashed="active === 'favorites'"
+            :quaternary="active !== 'favorites'"
+          >
             <n-icon size="20">
               <Heart />
             </n-icon>
           </n-button>
         </template>
-        <span>{{ $t('favorites') }}</span>
+        <span>{{ $t("favorites") }}</span>
       </n-popover>
       <n-popover trigger="hover" placement="right">
         <template #trigger>
-          <n-button circle type="primary" size="large" @click="goView('history')" :dashed="active === 'history'"
-            :quaternary="active !== 'history'">
+          <n-button
+            circle
+            type="primary"
+            size="large"
+            @click="goView('history')"
+            :dashed="active === 'history'"
+            :quaternary="active !== 'history'"
+          >
             <n-icon size="20"><Time /></n-icon>
           </n-button>
         </template>
-        <span>{{ $t('history') }}</span>
+        <span>{{ $t("history") }}</span>
       </n-popover>
       <n-popover trigger="hover" placement="right">
         <template #trigger>
-          <n-button circle type="primary" size="large" @click="goView('download')" :dashed="active === 'download'"
+          <n-button
+            circle
+            type="primary"
+            size="large"
+            @click="goView('download')"
+            :dashed="active === 'download'"
             v-show="false"
-            :quaternary="active !== 'download'">
+            :quaternary="active !== 'download'"
+          >
             <n-icon size="20">
               <ArrowDownCircleOutline />
             </n-icon>
           </n-button>
         </template>
-        <span>{{ $t('download') }}</span>
+        <span>{{ $t("download") }}</span>
       </n-popover>
     </div>
     <div class="bottom">
+      <div>
+        <n-avatar
+          class="avatar"
+          round
+          size="large"
+          :src="require('../assets/img/defaultavatar.png')"
+          @click="showRegister = true"
+        />
+        <n-modal v-model:show="showRegister">
+          <Register></Register>
+        </n-modal>
+      </div>
       <n-popover trigger="hover" placement="right">
         <template #trigger>
           <n-button quaternary circle type="primary" size="large">
@@ -77,61 +125,94 @@
             </n-icon>
           </n-button>
         </template>
-        <span>{{ $t('notifications') }}</span>
+        <span>{{ $t("notifications") }}</span>
       </n-popover>
       <n-popover trigger="hover" placement="right-end">
         <template #trigger>
-          <n-button circle type="primary" size="large"
-            :quaternary="!['settings', 'siteManager', 'liveManager'].includes(active)"
-            :dashed="['settings', 'siteManager', 'liveManager'].includes(active)">
+          <n-button
+            circle
+            type="primary"
+            size="large"
+            :quaternary="
+              !['settings', 'siteManager', 'liveManager'].includes(active)
+            "
+            :dashed="
+              ['settings', 'siteManager', 'liveManager'].includes(active)
+            "
+          >
             <n-icon size="20">
               <Menu />
             </n-icon>
           </n-button>
         </template>
         <template #default>
-          <n-button @click="goView('settings')" text><template #icon>
+          <n-button @click="goView('settings')" text
+            ><template #icon>
               <n-icon size="16">
                 <Settings />
-              </n-icon>
-            </template>Settings</n-button>
-          <div style="height: 10px;"></div>
-          <n-button @click="goView('siteManager')" text><template #icon>
+              </n-icon> </template
+            >Settings</n-button
+          >
+          <div style="height: 10px"></div>
+          <n-button @click="goView('siteManager')" text
+            ><template #icon>
               <n-icon size="16">
                 <ConstructOutline />
-              </n-icon>
-            </template>Site Manager</n-button>
-          <div style="height: 10px;"></div>
-          <n-button @click="goView('liveManager')" text><template #icon>
+              </n-icon> </template
+            >Site Manager</n-button
+          >
+          <div style="height: 10px"></div>
+          <n-button @click="goView('liveManager')" text
+            ><template #icon>
               <n-icon size="16">
                 <Build />
-              </n-icon>
-            </template>Live Manager</n-button>
-          <div style="height: 10px;"></div>
-          <n-button @click="goView('about')" text><template #icon>
+              </n-icon> </template
+            >Live Manager</n-button
+          >
+          <div style="height: 10px"></div>
+          <n-button @click="goView('about')" text
+            ><template #icon>
               <n-icon size="16">
                 <WaterOutline />
-              </n-icon>
-            </template>About</n-button>
+              </n-icon> </template
+            >About</n-button
+          >
         </template>
       </n-popover>
     </div>
   </div>
 </template>
 <script lang="ts" setup>
-import { PrismSharp, Tv, Play, Heart, Time, ArrowDownCircleOutline, Notifications, Settings, Menu, ConstructOutline, Build, WaterOutline } from '@vicons/ionicons5'
+import {
+  PrismSharp,
+  Tv,
+  Play,
+  Heart,
+  Time,
+  ArrowDownCircleOutline,
+  Notifications,
+  Settings,
+  Menu,
+  ConstructOutline,
+  Build,
+  WaterOutline
+} from '@vicons/ionicons5'
 import { useRouter, useRoute } from 'vue-router'
+import Register from './Register.vue'
 
 const show = ref(true)
 const router = useRouter()
 const route = useRoute()
 const active = ref<string>('discovery')
-
-watch(() => route.name, (name) => {
-  if (typeof name === 'string') {
-    active.value = name
+const showRegister = ref(false)
+watch(
+  () => route.name,
+  (name) => {
+    if (typeof name === 'string') {
+      active.value = name
+    }
   }
-})
+)
 
 function goView (v: string) {
   router.push({ name: v })
@@ -165,7 +246,12 @@ function goView (v: string) {
     display: flex;
     flex-direction: column;
     align-items: center;
-
+    .avatar {
+      -webkit-app-region: no-drag;
+      cursor: pointer;
+      padding: 2px;
+      box-sizing: border-box;
+    }
     button {
       margin-top: 10px;
     }
---
+++ src/renderer/components/Register.vue
@@ -0,0 +1,87 @@
+<template>
+  <n-card
+    style="width: 400px"
+    title="注册"
+    :bordered="false"
+    size="huge"
+    role="dialog"
+    aria-modal="true"
+  >
+    <n-form
+      ref="formRef"
+      :label-width="80"
+      :model="formValue"
+      :rules="rules"
+    >
+      <n-form-item label="姓名" path="user.name">
+        <n-input v-model:value="formValue.username" placeholder="输入用户名" />
+      </n-form-item>
+      <n-form-item label="年龄" path="user.age">
+        <n-input
+          v-model:value="formValue.password"
+          type="password"
+          show-password-on="click"
+          placeholder="自定义密码图标"
+          :maxlength="8"
+        >
+          <template #password-visible-icon>
+            <n-icon :size="16" :component="GlassesOutline" />
+          </template>
+          <template #password-invisible-icon>
+            <n-icon :size="16" :component="Glasses" />
+          </template>
+        </n-input>
+      </n-form-item>
+      <n-form-item label="电话号码" path="phone">
+        <n-input v-model:value="formValue.email" placeholder="邮箱" />
+      </n-form-item>
+      <n-form-item label="电话号码" path="phone">
+        <n-input v-model:value="formValue.vCode" placeholder="验证码" />
+      </n-form-item>
+      <n-form-item>
+        <n-button attr-type="button" @click="handleValidateClick">
+          验证
+        </n-button>
+      </n-form-item>
+    </n-form>
+  </n-card>
+</template>
+<script lang="ts" setup>
+import type { NForm } from 'naive-ui'
+import { GlassesOutline, Glasses } from '@vicons/ionicons5'
+import { RegisterType } from '../api/types/user.type'
+const formRef = ref<InstanceType<typeof NForm>>()
+const rules = reactive({
+  username: {
+    required: true,
+    message: '请输入用户名',
+    trigger: ['input']
+  },
+  password: {
+    required: true,
+    message: '请输入用户名',
+    trigger: ['input']
+  },
+  email: {
+    required: true,
+    message: '请输入用户名',
+    trigger: ['input']
+  },
+  vCode: {
+    required: true,
+    message: '请输入用户名',
+    trigger: ['input']
+  }
+})
+const formValue = reactive<RegisterType>({
+  username: '',
+  password: '',
+  email: '',
+  vCode: ''
+})
+function handleValidateClick () {
+  //
+}
+</script>
+
+<style lang="scss" scoped></style>
